14fda60d1678bfff7ed6e8404549d741bc3e0721
iftakhar <iftakharalamshuvo11@gmail.com>
2024-01-29 23:11:42 +0600

    some added

--- while/2 odd even.js
+++ while/2 odd even.js
@@ -5,6 +5,13 @@ Subtask-1:
 Find all the odd numbers from 61 to 100.
 
  */
+let numbers =61;
+while (numbers<=100) {
+    if (numbers%2!==0) {
+        console.log(`this is a odd number ${numbers}`);
+    }
+    numbers++
+}
 
 
 /***
@@ -14,5 +21,13 @@ Subtask-2:
 Find all the even numbers from 78 to 98.
 
  */
+let num=78;
+while (num<=98) {
+    if (num%2===0) {
+        console.log(`this is a even number ${num}`);
+    }
+    num++
+}
+
 
 /*programming hero*/
