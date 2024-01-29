105a57c71a6a3b40101cba861db9dd22e50d0f79
iftakhar <iftakharalamshuvo11@gmail.com>
2024-01-29 23:56:02 +0600

    multification

--- while/4 multiplication table.js
+++ while/4 multiplication table.js
@@ -3,6 +3,14 @@
 As Ersa is learning now, she wants to explore more and more. Tell Ersa to generate a multiplication table for number 5
 
  */
+let num=0;
+let tableNumber=5;
+let result;
+while (num<=10&&num>=0) {
+    result =num * tableNumber;
+    console.log(`${tableNumber} * ${num} =${result}`);
+    num++
+}
 
 
 /*programming hero*/
