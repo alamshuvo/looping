b0d3d06ee476513f60e27348c1000092eaffedd4
iftakhar <iftakharalamshuvo11@gmail.com>
2024-01-30 00:00:28 +0600

    countdown

--- while/5 countdown.js
+++ while/5 countdown.js
@@ -3,6 +3,11 @@
 Implement a countdown timer that counts down from 21 to 15.
 
  */
-
+let countdown =21;
+while (countdown>=15) {
+    console.log(countdown);
+    countdown--;
+}
+console.log("conuntdown copleted !");
 
 /*programming hero*/
