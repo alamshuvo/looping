bd4bd681d4339579013292cd2f84e37dd7667f48
iftakhar <iftakharalamshuvo11@gmail.com>
2024-01-30 00:09:52 +0600

    odd even

--- for/2 odd even.js
+++ for/2 odd even.js
@@ -5,7 +5,12 @@ Subtask-1:
 Find all the odd numbers from 61 to 100.
 
  */
-
+for (let i = 61; i <=100; i++) {
+    if (i%2!==0) {
+        console.log(`this is odd number ${i}`);
+    }
+    
+}
 
 /***
 
@@ -14,5 +19,11 @@ Subtask-2:
 Find all the even numbers from 78 to 98.
 
  */
+for (let i = 78; i <=98; i++) {
+    if (i%2===0) {
+        console.log(`this is even number ${i}`);
+    }
+    
+}
 
 /*programming hero*/
