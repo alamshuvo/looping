5b9c59319ca7cce8ae70012a684c7d575d458ee1
iftakhar <iftakharalamshuvo11@gmail.com>
2024-01-30 00:25:31 +0600

    multification of 9

--- for/4 multiplication table.js
+++ for/4 multiplication table.js
@@ -3,6 +3,10 @@
 Generate a multiplication table for number 9
 
  */
-
+let number=9;
+for (let i = 0; i <=10; i++) {
+    let result=i*number;
+    console.log(`${number} * ${i} = ${result}`); 
+}
 
 /*programming hero*/
