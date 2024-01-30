068f27ef3d5ca7e8a8a87dd76834cec00f2e98e9
iftakhar <iftakharalamshuvo11@gmail.com>
2024-01-30 18:45:07 +0600

    break and continue

--- break/1 century.js
+++ break/1 century.js
@@ -3,3 +3,36 @@
 Write a loop 1 to 200. Use break to exit the loop once you find 100.
 
 */
+
+for (let i = 1; i<=200; i++) {
+    console.log(i);
+    if (i==100) {
+        break;
+    }  
+}
+console.log("This is century my retierment");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const array = [1, 2, 3, 4, 5, 6, 7];
+
+// for (let i = 1; i < array.length; i++) {
+//  if (i == 5) {
+//   continue;
+//  }
+//  console.log(array[i]);
+
+// }
